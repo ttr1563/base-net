1da4d6fed0a35c8ff488daa646fd05a8df618aa6
yamadamizuki <yamada2@skiyaki.com>
2019-07-19 11:28:03 +0900

    [master]ベースの作成

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# base-net
+# ryus noodle bar
+カナダ案件
+(https://docs.google.com/spreadsheets/d/1_ohW30yyvnaktwgOdt-yRLicA0tLu2oaL018zmCN0KE/edit#gid=0)[案件内容]
+(https://drive.google.com/drive/u/1/folders/17_Yr82XdCZbBTyHU0QSg4_jIYBZoPrUl)[デザイン]
+
+
---
+++ ryusnoodlebar/no_content.php
@@ -0,0 +1,18 @@
+<main class="no-content">
+    <div class="container">
+        <p class="lead glitch" data-text="Not Fount">Not Found</p>
+        <p class="lead-bottom glitch" data-text="お探しのページは見つかりませんでした。">お探しのページは見つかりませんでした。</p>
+    </div>
+    <footer id="footer">
+        <div class="footer-upper">
+            <h1 class="tour-logo">
+                <img src="./dist/images/tour_logo.png" alt="MAN WITH A MISSION PRESENTS CHASING THE HORIZON WORLD TOUR2018/2019 JAPAN EXTRA SHOWS">
+            </h1>
+        </div>
+        <p class="footer-logo">
+            <img src="./dist/images/foooter_icon.png" alt="MAN WITH A MISSION">
+        </p>
+        <p class="copyright">https://www.mwamjapan.info/</p>
+    </footer>
+</main>
+<script src="./dist/js/footerFixed.js" type="text/javascript" charset="utf-8"></script>
